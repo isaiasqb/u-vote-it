42373f69ff6e47db67921a7c2f95011a2344ac85
IsaiasQB <isaias.qb@hotmail.com>
2023-07-17 20:27:33 -0400

    added Post, Delete and Get api callss to the server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import express
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 //add PORT designation & app expression
 const PORT = process.env.PORT || 3001;
@@ -29,37 +30,85 @@ app.get('/', (req, res) => {
   })
 });
 
-// TEST Query for the database
-db.query(`SELECT * FROM supremes`, (err, rows) => {
-  console.log(rows)
+// GET ALL the supremes from the database
+app.get('/api/supremes', (req, res) => {
+  const sqlCommand =`SELECT * FROM supremes`;
+  db.query(sqlCommand, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    })
+  })
 })
+
 
 // GET a SINGLE supreme
-// db.query(`SELECT * FROM supremes WHERE id = 5`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row)
-// });
+app.get('/api/supreme/:id', (req, res) => {
+  const sqlCommand = `SELECT * FROM supremes WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sqlCommand, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    })
+  });
+})
 
 // DELETE a single CANDIDATE
-// db.query(`DELETE FROM supremes WHERE id = ?`, 1, (err, result) => {
-//   if (err){
-//     console.log(err);
-//   }
-//   console.log(result)
-// })
+app.delete('/api/supreme/:id', (req, res) => {
+  const sqlCommand = `DELETE FROM supremes WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sqlCommand, params, (err, result) => {
+    if (err){
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Supreme is not in the database'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
 // CREATE another supreme candidate
-const sql = `INSERT INTO supremes (id, first_name, last_name, royal_lineage)
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronaleria', 'Firbellink', 1];
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err)
+app.post('/api/supreme', ({body}, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'royal_lineage');
+  if (errors) {
+    res.status(400).json({ error: errors, message: "Not formated correctly"});
+    return
   }
-  console.log(result)
-})
+
+  const sqlCommand = `INSERT INTO supremes (first_name, last_name, royal_lineage) VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.royal_lineage];
+
+  db.query(sqlCommand, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+
 
 
 // DEFAULT reponse for any other requests (Not Found) - ALWAYS last call or it will override others
