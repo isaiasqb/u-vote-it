81e44da3cefaed23eb0e960a298d1a0b1255d841
IsaiasQB <isaias.qb@hotmail.com>
2023-07-18 22:29:47 -0400

    added new table for the different orders, modified schema and seeds sql files

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,17 @@
+DROP TABLE IF EXISTS supremes;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE orders (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR (50) NOT NULL,
+  creed_declaration TEXT
+);
+
 CREATE TABLE supremes (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  royal_lineage BOOLEAN NOT NULL
+  royal_lineage BOOLEAN NOT NULL,
+  order_id INTEGER,
+  CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE SET NULL
 );
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,12 +1,20 @@
-INSERT INTO supremes (first_name, last_name, royal_lineage)
+INSERT INTO orders (name, creed_declaration)
+VALUES 
+  ('The Willow Singers', 'Like others before me, I have the gift. But I choose to share it, depending on the light.'),
+  ('The Night Embers', 'You have always had the power. You find it within yourself. You find it amongst your kind.'),
+  ('The Limp Collectors', 'I am as powerful as the force I am up against. I take what I deserve from those who have wronged me.'),
+  ('The Veiled Furies', 'I was born in darkness, I revel in the shadows. My face is not one you can see outside your nightmares.'),
+  ('The Oceanid Daughters', 'A witch is the embodiment of demonic feminity. She has as many faces as the Moon.');
+INSERT INTO supremes (first_name, last_name, royal_lineage, order_id)
 VALUES
-  ('Colmena', 'Firbank', 1),
-  ('Virginia', 'Woolfrig', 1),
-  ('Reena', 'Gaveston', 0),
-  ('Charmelia', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edwina', 'Bellamy', 0),
-  ('Montalva', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+  ('Colmena', 'Firbank', 1, 1),
+  ('Virginia', 'Woolfrig', 1, 2),
+  ('Reena', 'Gaveston', 0, 2),
+  ('Charmelia', 'LeRoi', 1, 4),
+  ('Katherine', 'Mansfield', 1, 5),
+  ('Dora', 'Carrington', 0, 3),
+  ('Edwina', 'Bellamy', 0, 4),
+  ('Montalva', 'Summers', 1, 3),
+  ('Octavia', 'Butler', 1, 5),
+  ('Unica', 'Zurn', 1, 2);
+
